cbdcae6aa696e760a8b2554102fc515bc4312834
Aiotex <79638013+Aiotex@users.noreply.github.com>
2026-05-16 16:50:49 +0300

    removed stale tanstack deps

---
+++ apps/expo/README.md
@@ -0,0 +1,10 @@
+
+# Getting started
+
+To make things work on your local simulator, or on your phone, you need first to [run `eas build`](https://github.com/infinitered/ignite/blob/master/docs/expo/EAS.md). There are many shortcuts on `package.json` to make it easier:
+
+```bash
+pnpm run build:ios:sim # build for ios simulator
+pnpm run build:ios:device # build for ios device
+pnpm run build:ios:prod # build for ios device
+```
---
+++ apps/expo/app.config.ts
@@ -0,0 +1,57 @@
+import type { ConfigContext, ExpoConfig } from 'expo/config'
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+	...config,
+	name: 'expo',
+	slug: 'insprii',
+	scheme: 'expo',
+	version: '0.1.0',
+	orientation: 'portrait',
+	icon: './assets/icon-light.png',
+	userInterfaceStyle: 'automatic',
+	updates: {
+		fallbackToCacheTimeout: 0,
+	},
+	newArchEnabled: true,
+	assetBundlePatterns: ['**/*'],
+	ios: {
+		bundleIdentifier: 'com.expo', // Note: Should follow the convenstion of com.yourcompany/yourname.yourappname
+		supportsTablet: true,
+		icon: './assets/images/app-icon-ios.png',
+	},
+	android: {
+		package: 'com.expo',
+		icon: './assets/images/app-icon-android-legacy.png',
+		adaptiveIcon: {
+			foregroundImage: './assets/images/app-icon-android-adaptive-foreground.png',
+			backgroundImage: './assets/images/app-icon-android-adaptive-background.png',
+		},
+		allowBackup: false,
+		edgeToEdgeEnabled: true,
+	},
+	extra: {
+		eas: {
+			projectId: '2077f102-5567-45d2-8bae-acca2ae52a60',
+		},
+	},
+	experiments: {
+		tsconfigPaths: true,
+		typedRoutes: true,
+		reactCanary: true,
+		reactCompiler: true,
+	},
+	plugins: [
+		'expo-router',
+		'expo-secure-store',
+		'expo-web-browser',
+		[
+			'expo-splash-screen',
+			{
+				image: './assets/images/app-icon-android-adaptive-foreground.png',
+				imageWidth: 300,
+				resizeMode: 'contain',
+				backgroundColor: '#191015',
+			},
+		],
+	],
+})
